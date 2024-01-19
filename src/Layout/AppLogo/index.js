27e81dcd3a872fa09702dcfaf9060af015a196cf
AgileWebDev92 <billpaxton92@gmail.com>
2024-01-19 14:53:59 +0900

    layout updated

--- src/Layout/AppLogo/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppLogo = () => {
-  return <></>;
-};
-
-export default AppLogo;
--- src/Layout/AppMain/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppMain = () => {
-  return <></>;
-};
-
-export default AppMain;
